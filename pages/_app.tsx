bf62eea3dc4cd7a72b2c15cd60ed10e485ad44fc
kailous <kailous@live.cn>
2023-09-25 11:29:05 +0800

    版本 2.7 增加了跟踪，方便分析来访数据。

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 // pages/_app.js
 
 import './css/globals.css'; // 引入全局样式
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+      <Component {...pageProps} />
+        <Analytics />
+      </>
+  );
 }
 
 export default MyApp;
